814b1678eb8e53962e4ac87522c893a7809c7c76
debzou <david.petit36@gmail.com>
2021-09-14 11:47:43 +0200

    update installation

--- start_project_first_time.sh
+++ start_project_first_time.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 docker-compose up --build
-docker exec -it mongo_ingestion ./start.sh
+docker exec -it mongo_ingestion mongoimport -d database_unita -c coord_map coord_map.json
+docker exec -it mongo_ingestion mongoimport -d database_unita --jsonArray -c data_map data_map.json 
+docker exec -it mongo_ingestion mongoimport -d database_unita -c data_thematic_sunburst data_thematic_sunburst.json 
+docker exec -it mongo_ingestion mongoimport -d database_unita -c data_univ_sunburst data_sun_univ.json  
+docker exec -it mongo_ingestion mongoimport -d database_unita -c data_table_courses --jsonArray data_table_courses.json
