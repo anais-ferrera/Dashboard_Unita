bb5b6843cd41d042ee7c60add4cd8a382dd967ff
debzou <david.petit36@gmail.com>
2021-09-21 15:28:35 +0200

    update script ingestion

--- start_project_first_time.sh
+++ start_project_first_time.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 mkdir mongodbdata
-docker-compose up --build
+docker-compose up --build -d
 docker exec -it mongo_ingestion mongoimport -d database_unita -c coord_map coord_map.json
 docker exec -it mongo_ingestion mongoimport -d database_unita --jsonArray -c data_map data_map.json 
 docker exec -it mongo_ingestion mongoimport -d database_unita -c data_thematic_sunburst data_thematic_sunburst.json 
